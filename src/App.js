b73b0637c912c03a38d744a658f8e4c6479ddb8e
Cobrettie <cobrettiegarner3@gmail.com>
2019-08-23 14:46:18 -0400

    operator buttons rendered to screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-import Numbers from '../src/components/ButtonComponents/NumberButtons/Numbers';
-import NumberButton from '../src/components/ButtonComponents/NumberButtons/NumberButton';
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+
+// STEP 4 - import the button and display components
+// Don't forget to import any extra css/scss files you build into the correct component
+import Numbers from '../src/components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -21,6 +22,7 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
+        <Operators />
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
- const NumberButton = (props) => {
+const NumberButton = (props) => {
   return (
     <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
